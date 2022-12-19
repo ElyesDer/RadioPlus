228aca4c9ad1be1bdcf3e75cdcadcc452a9c6c43
Elyes <lyesderouich@gmail.com>
2022-12-19 18:57:47 +0100

    Initial Commit

---
+++ RadioPlus/RadioPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioPlusApp.swift
+//  RadioPlus
+//
+//  Created by Derouiche Elyes on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RadioPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
